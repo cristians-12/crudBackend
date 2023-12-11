7f2c32bc288cad082112baff83abedc240e2052a
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-12-11 15:40:53 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     try {
-     await mongoose.connect(process.env.MONGO_URI);
+     await mongoose.connect("mongodb+srv://cristians12:bhThSIJCnp3tvhXD@crud.qitybow.mongodb.net/?retryWrites=true&w=majority");
      console.log("Conectado a la base de datos.")   
     } catch (error) {
         console.log(error)
