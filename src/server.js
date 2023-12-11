29854a9c31466825b3a5185d3ef60cefe19a2235
Cristian <97061228+cristians-12@users.noreply.github.com>
2023-12-11 15:55:25 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://frontend-login-pi.vercel.app"],
+    origin: ["http://localhost:5173", "https://crud12.vercel.app"],
     credentials: true,
   })
 );
